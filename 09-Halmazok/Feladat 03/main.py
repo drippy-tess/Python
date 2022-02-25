77a23e09e0f053b3ce513b8e5e40bf92ecda5fa7
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-02-25 09:39:15 +0100

    Python

--- 09-Halmazok/Feladat 03/main.py
+++ 09-Halmazok/Feladat 03/main.py
@@ -6,6 +6,7 @@ dobasokOsszege: int = None
 dobasokAtlaga: float = None
 hatosDobasokSzama: int = None
 paratlanDobasokSzama: int = None
+legtobbetEloforduloSzamok: List[int] = []
 
 def dobas(dobasokSzama: int = 7) -> List[int]:
     eredmeny: List[int] = []
@@ -20,16 +21,16 @@ def dobas(dobasokSzama: int = 7) -> List[int]:
 def dobasOsszeg(bejarandoHalmaz: List[int]) -> int:
     eredmeny: int = 0
 
-    for dobasok in bejarandoHalmaz:
-        eredmeny += dobasok
+    for dobas in bejarandoHalmaz:
+        eredmeny += dobas
 
     return eredmeny
 
 def hatosDobasSzam(bejarandoHalmaz: List[int]) -> int:
     eredmeny: int = 0
     
-    for dobasok in bejarandoHalmaz:
-        if(dobasok == 6):
+    for dobas in bejarandoHalmaz:
+        if(dobas == 6):
             eredmeny += 1
     
     return eredmeny
@@ -37,10 +38,40 @@ def hatosDobasSzam(bejarandoHalmaz: List[int]) -> int:
 def paratlanDobasSzam(bejarandoHalmaz: List[int]) -> int:
     eredmeny: int = 0
 
-    for dobasok in bejarandoHalmaz:
-        if(dobasok % 2 != 0):
+    for dobas in bejarandoHalmaz:
+        if(dobas % 2 != 0):
             eredmeny += 1
     
+    return eredmeny
+
+def legnagyobbKulcsErtek(szotar: Dict[int, int]) -> List[int]:
+    kulcs: int = None
+    ertek: int = 0
+    eredmeny: List[int] = []
+
+    #a legnagyobb érték kikeresése a szótárból
+    for key, value in szotar.items(): #végiglépkedünk a szótár össze elemén kulcs-érték párokkal
+        if(szotar[key] > ertek):
+            kulcs = key
+            ertek = szotar[key]
+
+    for key, value in szotar.items(): #kikeressük azokat a kulcsokat melyeknek az értéke egyenlő az érték változóval, mivel azok a kulcsok(dobások) száma fordul elő legtöbbször
+        if(szotar[key] == ertek):
+            eredmeny.append(key)
+    
+    return eredmeny
+
+def legtobbetEloforduloSzam(bejarandoHalmaz: List[int]) -> List[int]:
+    szotar: Dict[int, int] = {} #Dict[kulcs -> szám, érték -> szám előfordulási száma]
+    eredmeny: List[int] = []
+   #meghatározzuk az előfordulási számokat
+    for szam in bejarandoHalmaz:
+        if(szam in szotar):
+           szotar[szam] += 1 #szotar [szam] -> a kulcshoz tartozó értéket adja vissza
+        else:
+            szotar[szam] = 1
+
+    eredmeny = legnagyobbKulcsErtek(szotar)
     return eredmeny
 
 dobasok = dobas()
@@ -57,3 +88,6 @@ print(f"A hatos dobások száma: {hatosDobasokSzama}")
 
 paratlanDobasokSzama = paratlanDobasSzam(dobasok)
 print(f"A páratlan dobások száma: {paratlanDobasokSzama}")
+
+legtobbetEloforduloSzamok = legtobbetEloforduloSzam(dobasok)
+print(f"A legtöbbet előforduló szám(ok): {legtobbetEloforduloSzamok}")
