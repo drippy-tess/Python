d3b4e7207b9f87d88a9d98b322b93e28058e68b6
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-04-04 10:37:56 +0200

    Python

--- 10-OPP/Feladat 02/main.py
+++ 10-OPP/Feladat 02/main.py
@@ -1,4 +1,8 @@
 from Motorkerekpar import Motorkerekpar
+from Tulajdonos import Tulajdonos
 
 motorKerekpar: Motorkerekpar = Motorkerekpar("Honda", "CB600F", "Hornet", 70, 176, 599)
 print(motorKerekpar)
+
+tulaj: Tulajdonos = Tulajdonos("Kovács János", "1990. október 20.", "Férfi", "Debrecen")
+print(tulaj)
