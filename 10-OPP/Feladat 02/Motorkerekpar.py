abf27e250137934d3a5f3250c6d16ebf019a9dc8
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-04-04 12:06:06 +0200

    Python

--- 10-OPP/Feladat 02/Motorkerekpar.py
+++ 10-OPP/Feladat 02/Motorkerekpar.py
@@ -1,5 +1,7 @@
+from Tulajdonos import Tulajdonos
+
 class Motorkerekpar:
-    def __init__(self, gyarto: str, tipus: str, kivitel: str, teljesitmeny: int, suly: int, kobcenti: int) -> None:
+    def __init__(self, gyarto: str, tipus: str, kivitel: str, teljesitmeny: int, suly: int, kobcenti: int, tulaj: Tulajdonos) -> None:
         super().__init__()
 
         self.gyarto = gyarto
@@ -8,6 +10,8 @@ class Motorkerekpar:
         self.teljesitmeny = teljesitmeny
         self.suly = suly
         self.kobcenti = kobcenti
+        self.tulaj = tulaj
 
     def __str__(self) -> str:
-        return f"A motor gyártója: {self.gyarto}\nTípusa: {self.tipus}\nKivitele: {self.kivitel}\nTeljesítménye: {self.teljesitmeny}KW\nSúlya: {self.suly}KG\nHengerűrtartalma: {self.kobcenti}cc"
+        return f"A motor gyártója: {self.gyarto}\nTípusa: {self.tipus}\nKivitele: {self.kivitel}\nTeljesítménye: {self.teljesitmeny}KW\nSúlya: {self.suly}KG\nHengerűrtartalma: {self.kobcenti}cc\n{self.tulaj}"
+
--- 10-OPP/Feladat 02/Tulajdonos.py
+++ 10-OPP/Feladat 02/Tulajdonos.py
@@ -5,7 +5,7 @@ class Tulajdonos:
         self.nev = nev
         self.szuletesiDatum = szuletesiDatum
         self.nem = nem
-        self. szuletesiHely = szuletesiHely
+        self.szuletesiHely = szuletesiHely
 
     def __str__(self) -> str:
         return f"A motorkerékpár tulajdonosa: {self.nev}\nSzületési helye, ideje: {self.szuletesiHely}, {self.szuletesiDatum}\nNeme: {self.nem}"
