ca3262c0215a0832346b0384c57c260972dad2da
drippy_tess <74898218+drippy-tess@users.noreply.github.com>
2022-04-11 10:33:08 +0200

    Python

--- 10-OPP/Feladat 04/Osztaly.py
+++ 10-OPP/Feladat 04/Osztaly.py
@@ -2,18 +2,14 @@ from typing import *
 from Tanulo import Tanulo
 
 class Osztaly:
-    def __init__(self,osztalyNeve: str, tanulo: List[Tanulo], tanulo1: List[Tanulo], tanulo2: List[Tanulo]) -> None:
+    def __init__(self,osztalyNeve: str, tanulok: List[Tanulo]) -> None:
         super().__init__()
 
         self.osztalyNeve: str = osztalyNeve
-        self.tanulo: List[Tanulo] = tanulo
-        self.tanulo1: List[Tanulo] = tanulo1
-        self.tanulo2: List[Tanulo] = tanulo2
-
-        halmaz: List[Tanulo] = []
-        halmaz.append(tanulo)
-        halmaz.append(tanulo1)
-        halmaz.append(tanulo2)
+        self.tanulok: List[Tanulo] = tanulok
     
     def __str__(self) -> str:
-        return f"Az osztály neve: {self.osztalyNeve}\n{self.tanulo}\n{self.tanulo1}\n{self.tanulo2}"
+        output: str = f"{self.osztalyNeve}\n"
+        for tanulo in self.tanulok:
+            output += f"\n{tanulo}"
+        return output
--- 10-OPP/Feladat 04/main.py
+++ 10-OPP/Feladat 04/main.py
@@ -1,9 +1,12 @@
+from typing import *
 from Tanulo import Tanulo
 from Osztaly import Osztaly
+
 
 tanulo: Tanulo = Tanulo("Kedd", "Krisztián", "2000. január 1.")
 tanulo1: Tanulo = Tanulo("Szerda", "Szabolcs", "2001. február 2.")
 tanulo2: Tanulo = Tanulo("Csütörtök", "Csaba", "2002. március 3.")
-osztaly: Osztaly = Osztaly("10.B", tanulo, tanulo1, tanulo2)
+halmaz: List[Tanulo] = [tanulo, tanulo1, tanulo2]
+osztaly: Osztaly = Osztaly("10.B", halmaz)
 
 print(osztaly)
